cf39d59d36069dedc5982925daa387777f9d2508
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-10 13:34:53 +0500

    Add FontAwesomeModule and faUser icon

--- src/BestGarden.UI/src/app/components/header/header.component.ts
+++ src/BestGarden.UI/src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  faUser = faUser;
 }

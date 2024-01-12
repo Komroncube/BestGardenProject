b938ca79742a9a350f02add07183df3d32b6173b
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-12 20:55:11 +0500

    Add Catalog model to BestGarden.Domain.Models

---
+++ src/BestGarden.Domain/Models/Catalog.cs
@@ -0,0 +1,8 @@
+﻿namespace BestGarden.Domain.Models;
+public class Catalog : BaseDomainEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string PlantSystem { get; set; }
+    public string Supplied { get; set; }
+}

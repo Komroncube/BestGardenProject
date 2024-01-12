4ad3be02164b308173c3035172f905cb71658ccb
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-12 20:55:30 +0500

    Add Product model to BestGarden.Domain.Models

---
+++ src/BestGarden.Domain/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BestGarden.Domain.Models;
+public class Product
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImagePath { get; set; }
+    public int CatalogId { get; set; }
+    public Catalog Catalog { get; set; }
+}

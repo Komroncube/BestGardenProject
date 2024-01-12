6d876b6776de27f3f7c0f9008a659c23082a8f2c
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-12 20:55:17 +0500

    Add Order model class

---
+++ src/BestGarden.Domain/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using BestGarden.Domain.Enums;
+
+namespace BestGarden.Domain.Models;
+public class Order : BaseDomainEntity
+{
+    public int UserId { get; set; }
+    public User User { get; set; }
+
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+    public DeliveryType DeliveryType { get; set; }
+    public OrderStatus OrderStatus { get; set; } = OrderStatus.Checking;
+}

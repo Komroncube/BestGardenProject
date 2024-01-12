fd4588da994109bf7a280dac595f00a121fcb1d0
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-12 20:55:24 +0500

    Add OrderItems model class

---
+++ src/BestGarden.Domain/Models/OrderItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestGarden.Domain.Models;
+public class OrderItems : BaseDomainEntity
+{
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}

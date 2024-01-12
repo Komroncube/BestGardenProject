14d1ca57054df8865778233de7c2a244fe81d0c6
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-12 20:55:04 +0500

    Add BasketItems model class

---
+++ src/BestGarden.Domain/Models/BasketItems.cs
@@ -0,0 +1,13 @@
+﻿namespace BestGarden.Domain.Models;
+
+public class BasketItems : BaseDomainEntity
+{
+    public int UserId { get; set; }
+    public User User { get; set; }
+
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}

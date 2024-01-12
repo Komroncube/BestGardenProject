8db5f49e504d65440cdeebc4446c6bb31395a87a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-12 20:55:39 +0500

    Add User model to BestGarden.Domain.Models

---
+++ src/BestGarden.Domain/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using BestGarden.Domain.Enums;
+
+namespace BestGarden.Domain.Models;
+public class User : BaseDomainEntity
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
+    public LoyaltyStatus LoyaltyDiscountStatus { get; set; } = LoyaltyStatus.None;
+    public bool IsAdmin { get; set; } = false;
+}

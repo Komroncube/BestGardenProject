924777a4e70c19d1d3f393429184b581b79c4783
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:27:46 +0500

    Add DeleteUserCommand and DeleteUserCommandHandler

---
+++ src/BestGarden.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestGarden.Application.UseCases.Users.Commands.DeleteUser;
+public class DeleteUserCommand : ICommand<bool>
+{
+    public int Id { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestGarden.Application.UseCases.Users.Commands.DeleteUser;
+public class DeleteUserCommandHandler : ICommandHandler<DeleteUserCommand, bool>
+{
+    private readonly IUserRepository _userRepository;
+
+    public DeleteUserCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return _userRepository.DeleteAsync(request.Id, cancellationToken);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}

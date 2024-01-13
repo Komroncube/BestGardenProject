d35f2fea57ef25fc5eb9cfb519fee7e6210e7b38
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:28:54 +0500

    Update project configuration and add application and infrastructure layers

--- src/BestGarden.API/Program.cs
+++ src/BestGarden.API/Program.cs
@@ -1,3 +1,6 @@
+using BestGarden.Application;
+using BestGarden.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationLayer();
+builder.Services.AddInfrastructureLayer(builder.Configuration);
 
 var app = builder.Build();
 

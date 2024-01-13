ec22fcd0e0050c14f88a14b95f918a88c7a46212
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 18:10:42 +0500

    Add OrderEntityConfiguration class for configuring the Order entity

---
+++ src/BestGarden.Infrastructure/Configurations/Entities/OrderEntityConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BestGarden.Infrastructure.Configurations.Entities;
+internal class OrderEntityConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable("Orders");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.UserId)
+            .IsRequired();
+
+        builder.Property(x => x.OrderDate)
+            .HasDefaultValue(DateTime.Now)
+            .IsRequired();
+
+        builder.Property(x => x.DeliveryType)
+            .IsRequired();
+
+        builder.Property(x => x.OrderStatus)
+            .IsRequired();
+
+        builder.HasMany(x => x.OrderItems)
+            .WithOne(x => x.Order)
+            .HasForeignKey(x => x.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.Orders)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}

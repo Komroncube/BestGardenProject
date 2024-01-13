f372549dd716ac2ecfe6ab611c4517c19a691b37
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 17:42:40 +0500

    basket relations added

--- src/BestGarden.Domain/Models/BasketItem.cs
+++ src/BestGarden.Domain/Models/BasketItem.cs
@@ -3,10 +3,10 @@
 public class BasketItem : BaseDomainEntity
 {
     public int UserId { get; set; }
-    public User User { get; set; }
+    public virtual User User { get; set; }
 
     public int ProductId { get; set; }
-    public Product Product { get; set; }
+    public virtual Product Product { get; set; }
 
     public int Quantity { get; set; }
     public decimal Price { get; set; }

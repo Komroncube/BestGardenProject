2024edd0cf7a63cb0ee498507903e68836e4c024
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:27:24 +0500

    Add UserDTO class

---
+++ src/BestGarden.Application/DTOs/Users/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using BestGarden.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestGarden.Application.DTOs.Users;
+public class UserDTO
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PhoneNumber { get; set; }
+    public LoyaltyStatus LoyaltyDiscountStatus { get; set; } = LoyaltyStatus.None;
+    public bool IsAdmin { get; set; }
+}

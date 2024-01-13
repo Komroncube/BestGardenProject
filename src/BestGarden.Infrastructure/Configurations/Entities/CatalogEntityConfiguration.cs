f405fc8e55968028b1153313edd094d268b22330
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 18:10:32 +0500

    Add CatalogEntityConfiguration for Catalog model

---
+++ src/BestGarden.Infrastructure/Configurations/Entities/CatalogEntityConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BestGarden.Infrastructure.Configurations.Entities;
+
+internal class CatalogEntityConfiguration : IEntityTypeConfiguration<Catalog>
+{
+    public void Configure(EntityTypeBuilder<Catalog> builder)
+    {
+        builder.ToTable("Catalogs");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.PlantSystem)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.HasMany(x => x.Products)
+            .WithOne(x => x.Catalog)
+            .HasForeignKey(x => x.CatalogId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}

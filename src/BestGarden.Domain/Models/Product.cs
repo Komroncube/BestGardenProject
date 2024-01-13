cc60161839cd395ee3cbc72f93718f2a0bbc26f6
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 17:44:09 +0500

    Update Product class with navigation properties

--- src/BestGarden.Domain/Models/Product.cs
+++ src/BestGarden.Domain/Models/Product.cs
@@ -6,5 +6,8 @@ public class Product : BaseDomainEntity
     public decimal Price { get; set; }
     public string ImagePath { get; set; }
     public int CatalogId { get; set; }
-    public Catalog Catalog { get; set; }
+    public virtual Catalog Catalog { get; set; }
+
+    public virtual ICollection<BasketItem> BasketItems { get; set; }
+    public virtual ICollection<OrderItem> OrderItems { get; set; }
 }

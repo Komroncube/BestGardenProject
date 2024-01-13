96114ec0a493e2ff420c9fab8f39319831f6d753
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:26:35 +0500

    Update GenericRepository with CancellationToken support

--- src/BestGarden.Infrastructure/Repositories/GenericRepository.cs
+++ src/BestGarden.Infrastructure/Repositories/GenericRepository.cs
@@ -12,44 +12,44 @@ internal abstract class GenericRepository<T> : IGenericRepository<T> where T : B
         _dbContext = dbContext;
 
     }
-    public async Task<T> GetAsync(int id)
+    public async Task<T> GetAsync(int id, CancellationToken cancellationToken)
     {
-        return await _dbContext.Set<T>().FindAsync(id);
+        return await _dbContext.Set<T>().FindAsync(id, cancellationToken);
     }
 
-    public async Task<IReadOnlyList<T>> GetAllAsync()
+    public async Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken)
     {
-        return await _dbContext.Set<T>().ToListAsync();
+        return await _dbContext.Set<T>().ToListAsync(cancellationToken);
     }
 
-    public async Task<bool> ExistsAsync(int id)
+    public async Task<bool> ExistsAsync(int id, CancellationToken cancellationToken)
     {
-        return await _dbContext.Set<T>().AnyAsync(e => e.Id == id);
+        return await _dbContext.Set<T>().AnyAsync(e => e.Id == id, cancellationToken: cancellationToken);
     }
 
-    public async Task<T> AddAsync(T entity)
+    public async Task<T> AddAsync(T entity, CancellationToken cancellationToken)
     {
         _dbContext.Set<T>().Add(entity);
-        await _dbContext.SaveChangesAsync();
+        await _dbContext.SaveChangesAsync(cancellationToken);
         return entity;
 
     }
 
-    public async Task<T> UpdateAsync(T entity)
+    public async Task<T> UpdateAsync(T entity, CancellationToken cancellationToken)
     {
         _dbContext.Entry(entity).State = EntityState.Modified;
-        await _dbContext.SaveChangesAsync();
+        await _dbContext.SaveChangesAsync(cancellationToken);
         return entity;
     }
 
-    public async Task<bool> DeleteAsync(int id)
+    public async Task<bool> DeleteAsync(int id, CancellationToken cancellationToken)
     {
-        var entity = await _dbContext.Set<T>().FindAsync(id);
+        var entity = await _dbContext.Set<T>().FindAsync(id, cancellationToken);
         if (entity == null)
         {
             return false;
         }
         _dbContext.Set<T>().Remove(entity);
-        return await _dbContext.SaveChangesAsync() > 0;
+        return await _dbContext.SaveChangesAsync(cancellationToken) > 0;
     }
 }

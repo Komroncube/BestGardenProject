08398934b324545434d933cd971bb25b563ad56a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:27:58 +0500

    Add UpdateUserCommand and UpdateUserCommandHandler

---
+++ src/BestGarden.Application/UseCases/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BestGarden.Application.DTOs.Users;
+using BestGarden.Domain.Enums;
+
+namespace BestGarden.Application.UseCases.Users.Commands.UpdateUser;
+public class UpdateUserCommand : ICommand<User>
+{
+    public int Id { get; set; }
+    public UserDTO UserDto { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace BestGarden.Application.UseCases.Users.Commands.UpdateUser;
+public class UpdateUserCommandHandler : ICommandHandler<UpdateUserCommand, User>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UpdateUserCommandHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+    public async Task<User> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            User user = await _userRepository.GetAsync(request.Id, cancellationToken);
+            if (user is null)
+            {
+                throw new Exception("User not found");
+            }
+            _mapper.Map(request.UserDto, user);
+            return await _userRepository.UpdateAsync(user, cancellationToken);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}

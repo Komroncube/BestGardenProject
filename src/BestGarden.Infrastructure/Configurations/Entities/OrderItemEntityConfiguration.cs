d1b60bd56dd5ae9b8e96e66a63b908fb28014d38
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 18:11:00 +0500

    Add OrderItemEntityConfiguration class

---
+++ src/BestGarden.Infrastructure/Configurations/Entities/OrderItemEntityConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BestGarden.Infrastructure.Configurations.Entities;
+internal class OrderItemEntityConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.ToTable("OrderItems");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.ProductId)
+            .IsRequired();
+
+        builder.Property(x => x.Quantity)
+            .IsRequired();
+
+        builder.Property(x => x.OrderId)
+            .IsRequired();
+
+        builder.HasOne(x => x.Product)
+            .WithMany(x => x.OrderItems)
+            .HasForeignKey(x => x.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Order)
+            .WithMany(x => x.OrderItems)
+            .HasForeignKey(x => x.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}

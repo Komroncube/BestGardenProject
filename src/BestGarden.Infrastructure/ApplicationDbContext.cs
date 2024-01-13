60d68e15d242e5da7101bb21f9de8e3559c9f4cd
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 18:13:51 +0500

    Add ApplicationDbContext class

---
+++ src/BestGarden.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestGarden.Infrastructure;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opions) : base(opions) { }
+
+    public virtual DbSet<BasketItem> BasketItems { get; set; }
+    public virtual DbSet<Catalog> Catalogs { get; set; }
+    public virtual DbSet<Order> Orders { get; set; }
+    public virtual DbSet<OrderItem> OrderItems { get; set; }
+    public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<User> Users { get; set; }
+}

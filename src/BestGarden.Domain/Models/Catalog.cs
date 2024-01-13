07da25a7479bc23cd9b320fa9c69265aa5b8ed4a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 17:43:49 +0500

    Update Catalog model to include Products collection

--- src/BestGarden.Domain/Models/Catalog.cs
+++ src/BestGarden.Domain/Models/Catalog.cs
@@ -4,5 +4,5 @@ public class Catalog : BaseDomainEntity
     public string Name { get; set; }
     public string Description { get; set; }
     public string PlantSystem { get; set; }
-    public string Supplied { get; set; }
+    public virtual ICollection<Product> Products { get; set; }
 }

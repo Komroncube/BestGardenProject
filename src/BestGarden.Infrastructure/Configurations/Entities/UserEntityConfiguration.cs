54d595ae2e1b90a8ab2082da3cbde6b43327afe7
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 18:11:19 +0500

    Add UserEntityConfiguration class for configuring User entity

---
+++ src/BestGarden.Infrastructure/Configurations/Entities/UserEntityConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain.Enums;
+using BestGarden.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BestGarden.Infrastructure.Configurations.Entities;
+
+internal class UserEntityConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.FullName)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.Email)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.Password)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.PhoneNumber)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.LoyaltyDiscountStatus)
+            .HasDefaultValue(LoyaltyStatus.None)
+            .IsRequired();
+
+        builder.Property(x => x.IsAdmin)
+            .HasDefaultValue(false)
+            .IsRequired();
+
+        builder.HasMany(x => x.Orders)
+            .WithOne(x => x.User)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(x => x.BasketItems)
+            .WithOne(x => x.User)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
+{
+}

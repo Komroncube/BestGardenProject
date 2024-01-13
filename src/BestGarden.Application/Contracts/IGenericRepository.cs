746a43604259ee9474441332c11cf2005584f5e3
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 16:21:13 +0500

    Add IGenericRepository interface

---
+++ src/BestGarden.Application/Contracts/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain;
+
+namespace BestGarden.Application.Contracts;
+public interface IGenericRepository<T> where T : BaseDomainEntity
+{
+    Task<T> GetAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<bool> ExistsAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(T entity);
+}

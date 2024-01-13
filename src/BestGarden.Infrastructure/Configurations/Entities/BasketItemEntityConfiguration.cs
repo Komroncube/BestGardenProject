b16c4556d15fd90c79ba80e9f96c57614dd5a903
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 18:42:19 +0500

    Update entity configuration files

--- src/BestGarden.Infrastructure/Configurations/Entities/BasketItemEntityConfiguration.cs
+++ src/BestGarden.Infrastructure/Configurations/Entities/BasketItemEntityConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BestGarden.Domain.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BestGarden.Infrastructure.Configurations.Entities;
--- src/BestGarden.Infrastructure/Configurations/Entities/UserEntityConfiguration.cs
+++ src/BestGarden.Infrastructure/Configurations/Entities/UserEntityConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BestGarden.Domain.Enums;
-using BestGarden.Domain.Models;
+﻿using BestGarden.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 

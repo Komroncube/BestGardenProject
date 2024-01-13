e7ba3f9c48a6743054e403ea1166dc4f6e70d536
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 16:21:32 +0500

    Add ICommand and IQuery interfaces for messaging

---
+++ src/BestGarden.Application/Messaging/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BestGarden.Application.Messaging;
+
+public interface ICommand<TResponse> : IRequest<TResponse>
+{
+}
+public interface ICommandHandler<TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+{ }
---
+++ src/BestGarden.Application/Messaging/IQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BestGarden.Application.Messaging;
+
+public interface IQuery<TResponse> : IRequest<TResponse>
+{
+}
+
+public interface IQueryHandler<TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{ }

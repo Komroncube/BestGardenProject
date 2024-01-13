f22bf23cffc2139f15cef7aead49507bd51380ba
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:27:30 +0500

    Add MappingProfile for UserDTO

---
+++ src/BestGarden.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BestGarden.Application.DTOs.Users;
+using BestGarden.Application.UseCases.Users.Commands.CreateUser;
+
+namespace BestGarden.Application.Profiles;
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<User, UserDTO>().ReverseMap();
+
+    }
+}

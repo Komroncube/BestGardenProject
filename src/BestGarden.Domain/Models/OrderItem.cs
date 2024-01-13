e7697a84d4fe1f797d5199d5a2aa94a5b361fc10
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 17:43:57 +0500

    Update OrderItem model to include OrderId and Order reference

--- src/BestGarden.Domain/Models/OrderItem.cs
+++ src/BestGarden.Domain/Models/OrderItem.cs
@@ -2,7 +2,9 @@
 public class OrderItem : BaseDomainEntity
 {
     public int ProductId { get; set; }
-    public Product Product { get; set; }
+    public virtual Product Product { get; set; }
     public int Quantity { get; set; }
     public decimal Price { get; set; }
+    public int OrderId { get; set; }
+    public virtual Order Order { get; set; }
 }

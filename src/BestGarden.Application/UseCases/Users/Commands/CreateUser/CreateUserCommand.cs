cd5298526904c3795b783e8f978ac3b8f778f7d4
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:27:40 +0500

    Add CreateUserCommand and CreateUserCommandHandler classes

---
+++ src/BestGarden.Application/UseCases/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Application.DTOs.Users;
+using BestGarden.Domain.Enums;
+
+namespace BestGarden.Application.UseCases.Users.Commands.CreateUser;
+public class CreateUserCommand : ICommand<User>
+{
+    public UserDTO UserDto { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace BestGarden.Application.UseCases.Users.Commands.CreateUser;
+public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, User>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public CreateUserCommandHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public Task<User> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        User user = _mapper.Map<User>(request.UserDto);
+        try
+        {
+            //password hashing
+
+            return _userRepository.AddAsync(user, cancellationToken);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}

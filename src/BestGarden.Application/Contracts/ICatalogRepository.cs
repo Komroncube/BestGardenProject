ffb9e05e34237cc9ea9f5b153509407281c78283
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 16:21:21 +0500

    Add repository interfaces for BasketItem, Catalog, OrderItem, Order, Product, and User

---
+++ src/BestGarden.Application/Contracts/ICatalogRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain.Models;
+
+namespace BestGarden.Application.Contracts;
+public interface ICatalogRepository : IGenericRepository<Catalog>
+{
+
+}
---
+++ src/BestGarden.Application/Contracts/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain.Models;
+
+namespace BestGarden.Application.Contracts;
+public interface IUserRepository : IGenericRepository<User>
+{
+}

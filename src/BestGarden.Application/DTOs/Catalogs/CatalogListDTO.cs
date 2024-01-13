ddd7bca4bfa0d64ce42688260df75bc03d12a7d3
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:56:46 +0500

    Add CatalogListDTO class

---
+++ src/BestGarden.Application/DTOs/Catalogs/CatalogListDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestGarden.Application.DTOs.Catalogs;
+public class CatalogListDTO
+{
+    public int Id { get; set; }
+    public int ProductQuantity { get; set; }
+}

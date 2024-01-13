ec63df231856d74d5d1c5f66d2e9d3fd5b011a1b
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 18:11:13 +0500

    Add ProductEntityConfiguration class for configuring the Product entity

---
+++ src/BestGarden.Infrastructure/Configurations/Entities/ProductEntityConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BestGarden.Infrastructure.Configurations.Entities;
+internal class ProductEntityConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.Price)
+            .IsRequired();
+
+        builder.Property(x => x.ImagePath)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.CatalogId)
+            .IsRequired();
+
+        builder.HasOne(x => x.Catalog)
+            .WithMany(x => x.Products)
+            .HasForeignKey(x => x.CatalogId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(x => x.OrderItems)
+            .WithOne(x => x.Product)
+            .HasForeignKey(x => x.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(x => x.BasketItems)
+            .WithOne(x => x.Product)
+            .HasForeignKey(x => x.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}

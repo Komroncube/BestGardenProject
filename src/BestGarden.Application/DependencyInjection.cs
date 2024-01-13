2d00e9ec2ff2a8c1aa5d4ebb53b134845cac3faf
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-13 21:28:26 +0500

    Add DependencyInjection class for application layer

---
+++ src/BestGarden.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BestGarden.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        return services;
+    }
+    
+}

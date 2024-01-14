23d2b7ec3eef284da082d755e82c78507bbbfcfa
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-14 17:11:56 +0500

    Add GetCatalogListQuery and GetCatalogListQueryHandler classes

---
+++ src/BestGarden.Application/UseCases/Catalogs/Queries/GetCatalogList/GetCatalogListQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Application.DTOs.Catalogs;
+
+namespace BestGarden.Application.UseCases.Catalogs.Queries.GetCatalogList;
+public class GetCatalogListQuery : IQuery<IEnumerable<CatalogListDTO>>
+{
+}
---
+++ src/BestGarden.Application/UseCases/Catalogs/Queries/GetCatalogList/GetCatalogListQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Application.DTOs.Catalogs;
+
+namespace BestGarden.Application.UseCases.Catalogs.Queries.GetCatalogList;
+public class GetCatalogListQueryHandler : IQueryHandler<GetCatalogListQuery, IEnumerable<CatalogListDTO>>
+{
+    private readonly ICatalogRepository _catalogRepository;
+
+    public GetCatalogListQueryHandler(ICatalogRepository catalogRepository)
+    {
+        _catalogRepository = catalogRepository;
+    }
+
+    public async Task<IEnumerable<CatalogListDTO>> Handle(GetCatalogListQuery request, CancellationToken cancellationToken)
+    {
+        var catalogs = await _catalogRepository.GetAllAsync(cancellationToken);
+        var catalogList = catalogs.Select(catalog => new CatalogListDTO
+        {
+            Id = catalog.Id,
+            ProductQuantity = catalog.Products.Count
+        });
+        return catalogList;
+    }
+}

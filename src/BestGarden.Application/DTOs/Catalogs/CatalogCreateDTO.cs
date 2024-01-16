a2d63e2f9714764b7c61413bc2f34b978c94bafe
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 13:10:47 +0500

    Add CatalogCreateDTO and CatalogDetailDTO classes

---
+++ src/BestGarden.Application/DTOs/Catalogs/CatalogCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BestGarden.Application.DTOs.Catalogs;
+
+public class CatalogCreateDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string PlantSystem { get; set; }
+}
---
+++ src/BestGarden.Application/DTOs/Catalogs/CatalogDetailDTO.cs
@@ -0,0 +1,12 @@
+﻿using BestGarden.Application.DTOs.Products;
+
+namespace BestGarden.Application.DTOs.Catalogs;
+
+public class CatalogDetailDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string PlantSystem { get; set; }
+    public IEnumerable<ProductListDTO> Products { get; set; }
+}

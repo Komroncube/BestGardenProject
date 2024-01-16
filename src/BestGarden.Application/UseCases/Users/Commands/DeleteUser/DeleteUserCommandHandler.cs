b51c7f43eda955f712f31182a276f5c53aeb9f30
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 18:05:52 +0500

    Refactor DeleteUserCommand and DeleteUserCommandHandler namespaces

--- src/BestGarden.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ src/BestGarden.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BestGarden.Application.UseCases.Users.Commands.DeleteUser;
+﻿namespace BestGarden.Application.UseCases.Users.Commands.DeleteUser;
 public class DeleteUserCommandHandler : ICommandHandler<DeleteUserCommand, bool>
 {
     private readonly IUserRepository _userRepository;

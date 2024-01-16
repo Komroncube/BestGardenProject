87d46c4084773536fc14a9bece9cb7e0a0b9bc53
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 17:52:23 +0500

    Add MediaHelper class for handling image names

---
+++ src/BestGarden.Application/Common/Helpers/MediaHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace BestGarden.Application.Common.Helpers;
+public static class MediaHelper
+{
+    public static string GetImageName(string filename)
+    {
+        var fileInfo = new FileInfo(filename);
+        string extension = fileInfo.Extension;
+        string name = "IMG_" + Guid.NewGuid() + extension;
+        return name;
+    }
+}

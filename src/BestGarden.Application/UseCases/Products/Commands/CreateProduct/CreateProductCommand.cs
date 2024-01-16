2418299b368ab82f3c5aa714b269f3789ed17f94
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 17:57:35 +0500

    Add CreateProductCommand and CreateProductCommandHandler

---
+++ src/BestGarden.Application/UseCases/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,7 @@
+﻿using BestGarden.Application.DTOs.Products;
+
+namespace BestGarden.Application.UseCases.Products.Commands.CreateProduct;
+public class CreateProductCommand : ICommand<Product>
+{
+    public ProductCreateDTO ProductCreateDto { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using BestGarden.Application.Services;
+
+namespace BestGarden.Application.UseCases.Products.Commands.CreateProduct;
+public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, Product>
+{
+    private readonly IProductRepository _productRepository;
+    private readonly IFileService _fileService;
+    private readonly IMapper _mapper;
+
+    public CreateProductCommandHandler(IProductRepository productRepository, IMapper mapper, IFileService fileService)
+    {
+        _productRepository = productRepository;
+        _mapper = mapper;
+        _fileService = fileService;
+    }
+
+    public async Task<Product> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        Product product = _mapper.Map<Product>(request.ProductCreateDto);
+        try
+        {
+            product.ImagePath = await _fileService.SaveFileAsync(request.ProductCreateDto.Image);
+            return await _productRepository.AddAsync(product, cancellationToken);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}

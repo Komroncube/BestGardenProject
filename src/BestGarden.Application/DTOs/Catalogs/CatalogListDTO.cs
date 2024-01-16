3d2c0e7b7a48d9e32b62ce69e8d83e1873e275c3
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 17:52:30 +0500

    Refactor CatalogListDTO namespace

--- src/BestGarden.Application/DTOs/Catalogs/CatalogListDTO.cs
+++ src/BestGarden.Application/DTOs/Catalogs/CatalogListDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BestGarden.Application.DTOs.Catalogs;
+﻿namespace BestGarden.Application.DTOs.Catalogs;
 public class CatalogListDTO
 {
     public int Id { get; set; }

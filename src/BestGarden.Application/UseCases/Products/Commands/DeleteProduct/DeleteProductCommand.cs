1ebd3d180f3a51906b45db1b4e604bde209b7dc4
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 17:57:42 +0500

    Add delete product command and handler

---
+++ src/BestGarden.Application/UseCases/Products/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace BestGarden.Application.UseCases.Products.Commands.DeleteProduct;
+public class DeleteProductCommand : ICommand<bool>
+{
+    public int Id { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace BestGarden.Application.UseCases.Products.Commands.DeleteProduct;
+public class DeleteProductCommandHandler : ICommandHandler<DeleteProductCommand, bool>
+{
+    private readonly IProductRepository _productRepository;
+
+    public DeleteProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public Task<bool> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return _productRepository.DeleteAsync(request.Id, cancellationToken);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}

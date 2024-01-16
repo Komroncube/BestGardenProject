decb69ed302e5fd8376fa29c1236ec89d3d817d2
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 17:57:09 +0500

    Refactor GetCatalogDetailQuery and GetCatalogDetailQueryHandler

--- src/BestGarden.Application/UseCases/Catalogs/Queries/GetCatalogDetail/GetCatalogDetailQueryHandler.cs
+++ src/BestGarden.Application/UseCases/Catalogs/Queries/GetCatalogDetail/GetCatalogDetailQueryHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BestGarden.Application.DTOs.Catalogs;
+﻿using BestGarden.Application.DTOs.Catalogs;
 using BestGarden.Application.DTOs.Products;
 
 namespace BestGarden.Application.UseCases.Catalogs.Queries.GetCatalogDetail;

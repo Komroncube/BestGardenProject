54a22123b6596c7c7f9390cb8a5d924cba6ad749
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 13:11:08 +0500

    Add GetCatalogDetailQuery and GetCatalogDetailQueryHandler classes

---
+++ src/BestGarden.Application/UseCases/Catalogs/Queries/GetCatalogDetail/GetCatalogDetailQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Application.DTOs.Catalogs;
+
+namespace BestGarden.Application.UseCases.Catalogs.Queries.GetCatalogDetail;
+public class GetCatalogDetailQuery : IQuery<CatalogDetailDTO>
+{
+    public int Id { get; set; }
+}
+
---
+++ src/BestGarden.Application/UseCases/Catalogs/Queries/GetCatalogDetail/GetCatalogDetailQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BestGarden.Application.DTOs.Catalogs;
+using BestGarden.Application.DTOs.Products;
+
+namespace BestGarden.Application.UseCases.Catalogs.Queries.GetCatalogDetail;
+public class GetCatalogDetailQueryHandler : IQueryHandler<GetCatalogDetailQuery, CatalogDetailDTO>
+{
+    private readonly ICatalogRepository _catalogRepository;
+
+    public GetCatalogDetailQueryHandler(ICatalogRepository catalogRepository)
+    {
+        _catalogRepository = catalogRepository;
+    }
+
+    public async Task<CatalogDetailDTO> Handle(GetCatalogDetailQuery request, CancellationToken cancellationToken)
+    {
+        Catalog catalog = await _catalogRepository.GetAsync(request.Id, cancellationToken);
+        CatalogDetailDTO catalogDetail = new CatalogDetailDTO
+        {
+            Id = catalog.Id,
+            Name = catalog.Name,
+            Description = catalog.Description,
+            PlantSystem = catalog.PlantSystem,
+            Products = catalog.Products.Select(product => new ProductListDTO
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                ImagePath = product.ImagePath
+                
+            })
+        };
+        return catalogDetail;
+    }
+}

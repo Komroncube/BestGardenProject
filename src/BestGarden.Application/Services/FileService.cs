cacd2e617788c64cf88d66fc4785ee9caaa9f022
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-16 17:52:59 +0500

    Add FileService and IFileService interfaces
    This commit adds the FileService class and IFileService interface to the project. The FileService class provides methods for saving and deleting files, while the IFileService interface defines the contract for these methods.

---
+++ src/BestGarden.Application/Services/FileService.cs
@@ -0,0 +1,40 @@
+﻿using BestGarden.Application.Common.Helpers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace BestGarden.Application.Services;
+
+public class FileService : IFileService
+{
+    private const string MEDIA = "media";
+    private const string IMAGES = "images";
+    private readonly string ROOTPATH;
+
+    public FileService(IWebHostEnvironment environment)
+    {
+        // ROOTPATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        ROOTPATH = environment.WebRootPath;
+    }
+
+
+    public async Task<string> SaveFileAsync(IFormFile file)
+    {
+
+        var fileName = MediaHelper.GetImageName(file.FileName);
+        var subPath = Path.Combine(MEDIA, IMAGES, fileName);
+        var path = Path.Combine(ROOTPATH, subPath);
+
+        await using var stream = new FileStream(path, FileMode.Create);
+        await file.CopyToAsync(stream);
+        return subPath;
+    }
+
+    public async Task<bool> DeleteFileAsync(string filePath)
+    {
+        string path = Path.Combine(ROOTPATH, filePath);
+        if (!File.Exists(path)) return false;
+
+        await Task.Run(() => File.Delete(path));
+        return true;
+    }
+}
---
+++ src/BestGarden.Application/Services/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace BestGarden.Application.Services;
+
+public interface IFileService
+{
+    Task<string> SaveFileAsync(IFormFile file);
+    Task<bool> DeleteFileAsync(string fileName);
+}

ab3642490649d8ec270ff6595f6fd6377d595443
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-17 21:39:49 +0500

    Add CreateBasketItemCommand and CreateBasketItemCommandHandler

---
+++ src/BestGarden.Application/UseCases/Baskets/Commands/CreateBasketItem/CreateBasketItemCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace BestGarden.Application.UseCases.Baskets.Commands.CreateBasketItem;
+public class CreateBasketItemCommand : ICommand<BasketItem>
+{
+    public int ProductId { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Baskets/Commands/CreateBasketItem/CreateBasketItemCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace BestGarden.Application.UseCases.Baskets.Commands.CreateBasketItem;
+public class CreateBasketItemCommandHandler : ICommandHandler<CreateBasketItemCommand, BasketItem>
+{
+    private readonly IBasketItemRepository _basketRepository;
+
+    public CreateBasketItemCommandHandler(IBasketItemRepository basketRepository)
+    {
+        _basketRepository = basketRepository;
+    }
+
+    public Task<BasketItem> Handle(CreateBasketItemCommand request, CancellationToken cancellationToken)
+    {
+        BasketItem basketItem = new()
+        {
+            UserId = 2,
+            ProductId = request.ProductId,
+        };
+        try
+        {
+            return _basketRepository.AddAsync(basketItem, cancellationToken);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}

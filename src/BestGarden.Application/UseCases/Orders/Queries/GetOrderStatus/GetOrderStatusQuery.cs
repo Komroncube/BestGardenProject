adccfc068ea322f20a1fe707bb508cc3c736724c
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-17 21:39:36 +0500

    Add GetOrderStatusQuery and GetOrderStatusQueryHandler

---
+++ src/BestGarden.Application/UseCases/Orders/Queries/GetOrderStatus/GetOrderStatusQuery.cs
@@ -0,0 +1,7 @@
+﻿using BestGarden.Domain.Enums;
+
+namespace BestGarden.Application.UseCases.Orders.Queries.GetOrderStatus;
+public class GetOrderStatusQuery : IQuery<OrderStatus>
+{
+    public int OrderId { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Orders/Queries/GetOrderStatus/GetOrderStatusQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using BestGarden.Domain.Enums;
+
+namespace BestGarden.Application.UseCases.Orders.Queries.GetOrderStatus;
+public class GetOrderStatusQueryHandler : IQueryHandler<GetOrderStatusQuery, OrderStatus>
+{
+    private readonly IOrderRepository _orderRepository;
+
+    public GetOrderStatusQueryHandler(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<OrderStatus> Handle(GetOrderStatusQuery request, CancellationToken cancellationToken)
+    {
+        Order order = await _orderRepository.GetAsync(request.OrderId, cancellationToken);
+        return order.OrderStatus;
+    }
+}

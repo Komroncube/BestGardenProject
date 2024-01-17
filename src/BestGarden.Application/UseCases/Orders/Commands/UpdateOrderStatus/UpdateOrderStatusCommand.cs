e43d88f769581fa77695214a3ed298436bd59df1
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-17 21:38:48 +0500

    Add UpdateOrderStatusCommand and UpdateOrderStatusCommandHandler

---
+++ src/BestGarden.Application/UseCases/Orders/Commands/UpdateOrderStatus/UpdateOrderStatusCommand.cs
@@ -0,0 +1,8 @@
+﻿using BestGarden.Domain.Enums;
+
+namespace BestGarden.Application.UseCases.Orders.Commands.UpdateOrderStatus;
+public class UpdateOrderStatusCommand : ICommand<Order>
+{
+    public int OrderId { get; set; }
+    public OrderStatus OrderStatus { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Orders/Commands/UpdateOrderStatus/UpdateOrderStatusCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using BestGarden.Application.UseCases.Users.Notifications.OrderCreated;
+using BestGarden.Domain.Enums;
+
+namespace BestGarden.Application.UseCases.Orders.Commands.UpdateOrderStatus;
+public class UpdateOrderStatusCommandHandler : ICommandHandler<UpdateOrderStatusCommand, Order>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMediator _mediator;
+
+    public UpdateOrderStatusCommandHandler(IOrderRepository orderRepository, IMediator mediator)
+    {
+        _orderRepository = orderRepository;
+        _mediator = mediator;
+    }
+
+    public async Task<Order> Handle(UpdateOrderStatusCommand request, CancellationToken cancellationToken)
+    {
+        Order order = await _orderRepository.GetAsync(request.OrderId, cancellationToken);
+        order.OrderStatus = request.OrderStatus;
+        if (order.OrderStatus == OrderStatus.Completed)
+        {
+            await _mediator.Publish(new OrderDeliveredNotification(order.UserId), cancellationToken);
+        }
+        return await _orderRepository.UpdateAsync(order, cancellationToken);
+    }
+}

9480969d33fd3286f97102e36c819fb378ac9765
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-17 21:39:13 +0500

    Add GetOrderListQuery and GetOrderListQueryHandler

---
+++ src/BestGarden.Application/UseCases/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,6 @@
+﻿using BestGarden.Application.DTOs.Orders;
+
+namespace BestGarden.Application.UseCases.Orders.Queries.GetOrderList;
+public class GetOrderListQuery : IQuery<IEnumerable<OrderDTO>>
+{
+}
---
+++ src/BestGarden.Application/UseCases/Orders/Queries/GetOrderList/GetOrderListQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using BestGarden.Application.DTOs.Orders;
+
+namespace BestGarden.Application.UseCases.Orders.Queries.GetOrderList;
+public class GetOrderListQueryHandler : IQueryHandler<GetOrderListQuery, IEnumerable<OrderDTO>>
+{
+    private readonly IOrderRepository _orderRepository;
+
+    public GetOrderListQueryHandler(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<IEnumerable<OrderDTO>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+    {
+        var orders = await _orderRepository.GetAllAsync(cancellationToken);
+        var orderList = orders.Select(order => new OrderDTO()
+        {
+            Id = order.Id,
+            OrderDate = order.OrderDate,
+            OrderStatus = order.OrderStatus,
+            TotalPrice = order.OrderItems.Sum(orderItem => orderItem.Price * orderItem.Quantity),
+
+        });
+
+        return orderList;
+    }
+}

07905fb9cf0bf1649dff21b8754d37c74b892fc5
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-17 21:29:10 +0500

    Add BasketItemDTO class

---
+++ src/BestGarden.Application/DTOs/BasketItems/BasketItemDTO.cs
@@ -0,0 +1,8 @@
+﻿using BestGarden.Application.DTOs.Products;
+
+namespace BestGarden.Application.DTOs.BasketItems;
+public class BasketItemDTO
+{
+    public ProductListDTO Product { get; set; }
+    public int Quantity { get; set; }
+}

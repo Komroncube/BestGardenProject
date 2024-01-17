50c138f13171b87a3cc2cb58d3fb33805e1cf5eb
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-17 21:40:20 +0500

    Add GetBasketDetailQuery and GetBasketDetailQueryHandler

---
+++ src/BestGarden.Application/UseCases/Baskets/Queries/GetBasketDetail/GetBasketDetailQuery.cs
@@ -0,0 +1,6 @@
+﻿using BestGarden.Application.DTOs.BasketItems;
+
+namespace BestGarden.Application.UseCases.Baskets.Queries.GetBasketDetail;
+public class GetBasketDetailQuery : IQuery<IEnumerable<BasketItemDTO>>
+{
+}
---
+++ src/BestGarden.Application/UseCases/Baskets/Queries/GetBasketDetail/GetBasketDetailQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using BestGarden.Application.DTOs.BasketItems;
+using BestGarden.Application.DTOs.Products;
+
+namespace BestGarden.Application.UseCases.Baskets.Queries.GetBasketDetail;
+public class GetBasketDetailQueryHandler : IQueryHandler<GetBasketDetailQuery, IEnumerable<BasketItemDTO>>
+{
+    private readonly IBasketItemRepository _basketRepository;
+    private readonly IMapper _mapper;
+
+    public GetBasketDetailQueryHandler(IBasketItemRepository basketRepository, IMapper mapper)
+    {
+        _basketRepository = basketRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<BasketItemDTO>> Handle(GetBasketDetailQuery request, CancellationToken cancellationToken)
+    {
+        var basketItems = await _basketRepository.GetAllAsync(cancellationToken);
+        var basketList = basketItems.Select(basket => new BasketItemDTO
+        {
+            Product = _mapper.Map<ProductListDTO>(basket.Product),
+            Quantity = basket.Quantity
+        });
+        return basketList;
+    }
+}

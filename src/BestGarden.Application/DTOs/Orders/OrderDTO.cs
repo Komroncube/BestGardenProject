979f045f0883d3df5c02ff0ccd8fcbbec4923e02
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-17 21:29:17 +0500

    Add OrderDTO class

---
+++ src/BestGarden.Application/DTOs/Orders/OrderDTO.cs
@@ -0,0 +1,10 @@
+﻿using BestGarden.Domain.Enums;
+
+namespace BestGarden.Application.DTOs.Orders;
+public class OrderDTO
+{
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public OrderStatus OrderStatus { get; set; }
+    public decimal TotalPrice { get; set; }
+}

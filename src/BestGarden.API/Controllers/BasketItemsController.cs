0d498c5fa769c781267732d7dbd291a3d0e7e494
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-17 21:40:39 +0500

    Add BasketItemsController to API

---
+++ src/BestGarden.API/Controllers/BasketItemsController.cs
@@ -0,0 +1,41 @@
+﻿using BestGarden.Application.UseCases.Baskets.Commands.CreateBasketItem;
+using BestGarden.Application.UseCases.Baskets.Commands.UpdateBasketItem;
+using BestGarden.Application.UseCases.Baskets.Queries.GetBasketDetail;
+
+namespace BestGarden.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BasketItemsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public BasketItemsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetBasketItems()
+    {
+        var getBasketItemsQuery = new GetBasketDetailQuery();
+        var basketItems = await _mediator.Send(getBasketItemsQuery);
+
+        return Ok(basketItems);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddBasketItem([FromBody] int productId)
+    {
+        var basketItem = await _mediator.Send(new CreateBasketItemCommand { ProductId = productId });
+
+        return Ok(basketItem);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateBasketItem([FromBody] int productId, int quantity)
+    {
+        var updateBasketItemCommand = new UpdateBasketItemCommand { ProductId = productId, Quantity = quantity };
+        BasketItem basketItem = await _mediator.Send(updateBasketItemCommand);
+        return Ok(basketItem);
+    }
+}

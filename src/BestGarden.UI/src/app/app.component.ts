65da0c78dbeaa1f1997c10e85aa7538e7f9312a7
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-18 21:44:12 +0500

    Add header, footer, catalog title, and product info card components

--- src/BestGarden.UI/src/app/app.component.ts
+++ src/BestGarden.UI/src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
+import { CatalogTitleComponent } from "./components/catalog-title-card/catalog-title-card.component";
+import { ProductInfoCardComponent } from "./components/product-info-card/product-info-card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, CatalogTitleComponent, ProductInfoCardComponent]
 })
 export class AppComponent {
   title = 'BestGarden.UI';
+
+  catalogTitle = {
+    title : "Catalog",
+    imagePath : "../../../assets/landscape.png",
+    quantity : 10
+  }
 }

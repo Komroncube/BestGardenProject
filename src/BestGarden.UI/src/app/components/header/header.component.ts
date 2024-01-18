b4fd57e99174fae880408583b09fc1869de71980
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-18 19:33:29 +0500

    Refactor header component styles

--- src/BestGarden.UI/src/app/components/header/header.component.ts
+++ src/BestGarden.UI/src/app/components/header/header.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUser} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  faUser = faUser;
+  
 }

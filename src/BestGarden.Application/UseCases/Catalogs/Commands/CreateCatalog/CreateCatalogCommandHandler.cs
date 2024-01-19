73e17418e2c2742e551d897e3b6d4f10a40506da
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-19 21:48:02 +0500

    Add file service to CreateCatalogCommandHandler

--- src/BestGarden.Application/UseCases/Catalogs/Commands/CreateCatalog/CreateCatalogCommandHandler.cs
+++ src/BestGarden.Application/UseCases/Catalogs/Commands/CreateCatalog/CreateCatalogCommandHandler.cs
@@ -1,13 +1,18 @@
-﻿namespace BestGarden.Application.UseCases.Catalogs.Commands.CreateCatalog;
+﻿using BestGarden.Application.Services;
+
+namespace BestGarden.Application.UseCases.Catalogs.Commands.CreateCatalog;
 public class CreateCatalogCommandHandler : ICommandHandler<CreateCatalogCommand, Catalog>
 {
     private readonly ICatalogRepository _catalogRepository;
     private readonly IMapper _mapper;
+    private readonly IFileService _fileService;
 
-    public CreateCatalogCommandHandler(ICatalogRepository catalogRepository, IMapper mapper)
+
+    public CreateCatalogCommandHandler(ICatalogRepository catalogRepository, IMapper mapper, IFileService fileService)
     {
         _catalogRepository = catalogRepository;
         _mapper = mapper;
+        _fileService = fileService;
     }
 
     public async Task<Catalog> Handle(CreateCatalogCommand request, CancellationToken cancellationToken)
@@ -15,6 +20,7 @@ public class CreateCatalogCommandHandler : ICommandHandler<CreateCatalogCommand,
         try
         {
             var catalog = _mapper.Map<Catalog>(request.CatalogCreateDto);
+            catalog.ImagePath = await _fileService.SaveFileAsync(request.CatalogCreateDto.Image);
             await _catalogRepository.AddAsync(catalog, cancellationToken);
             return catalog;
         }

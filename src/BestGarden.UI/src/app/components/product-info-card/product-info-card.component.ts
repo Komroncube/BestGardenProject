4ace3c7f62cd1a4093ac7c91c855db4e0424dd33
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-19 21:49:58 +0500

    Update image path in product-info-card component

--- src/BestGarden.UI/src/app/components/product-info-card/product-info-card.component.ts
+++ src/BestGarden.UI/src/app/components/product-info-card/product-info-card.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { ProductInfo } from '../../Interfaces/Products/ProductInfo';
+import { ImageUrlPipe } from '../../pipes/image-url.pipe';
 
 @Component({
-  selector: 'app-product-info-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './product-info-card.component.html',
-  styleUrl: './product-info-card.component.scss'
+    selector: 'app-product-info-card',
+    standalone: true,
+    templateUrl: './product-info-card.component.html',
+    styleUrl: './product-info-card.component.scss',
+    imports: [ImageUrlPipe]
 })
 export class ProductInfoCardComponent {
   @Input()

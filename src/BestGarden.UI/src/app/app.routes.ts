5a01126ae597a626f4784efe10c79d9358d33c9a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-19 15:38:56 +0500

    Add PlaygroundComponent to app.routes.ts

--- src/BestGarden.UI/src/app/app.routes.ts
+++ src/BestGarden.UI/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PlaygroundComponent } from './pages/playground/playground.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: '', component: PlaygroundComponent}
 ];

ebfa672bc13d6835cad0081bf9f6ec4dff60d02e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-19 21:47:40 +0500

    Add ImagePath property to Catalog model

--- src/BestGarden.Domain/Models/Catalog.cs
+++ src/BestGarden.Domain/Models/Catalog.cs
@@ -4,5 +4,6 @@ public class Catalog : BaseDomainEntity
     public string Name { get; set; }
     public string Description { get; set; }
     public string PlantSystem { get; set; }
+    public string ImagePath { get; set; }
     public virtual ICollection<Product> Products { get; set; }
 }

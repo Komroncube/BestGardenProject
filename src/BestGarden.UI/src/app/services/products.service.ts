f6eb52c3f59a047dd83bf1d6578e1c14ee340fa1
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-19 19:28:00 +0500

    Add product service and product interface

---
+++ src/BestGarden.UI/src/app/services/products.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IProduct } from '../Interfaces/Products/IProduct';
+import { IProductService } from '../Interfaces/products.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService implements IProductService {
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * 
+   * @returns array of products
+   */
+  getProducts() {
+    return this.http.get<Array<IProduct>>('api/products');
+  }
+  getProductById(id: number) {
+    return this.http.get<IProduct>(`api/products/${id}`);
+  }
+}

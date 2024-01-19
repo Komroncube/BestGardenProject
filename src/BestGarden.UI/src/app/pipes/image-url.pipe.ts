37dac462374c4d7eb35fcb3d074c86ef9ddc3953
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-19 21:49:18 +0500

    Add ImageUrlPipe for transforming image URLs

---
+++ src/BestGarden.UI/src/app/pipes/image-url.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Pipe({
+  name: 'imageUrl',
+  standalone: true
+})
+export class ImageUrlPipe implements PipeTransform {
+  transform(value: string): string {
+      return `${environment.apiUrl}\\${value}`;
+  }
+}

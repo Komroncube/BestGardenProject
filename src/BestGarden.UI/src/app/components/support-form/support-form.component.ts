5e0dcf5528625ef04fd402afd93f33bc2faccd0b
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-19 15:38:13 +0500

    Add support form component with HTML, SCSS, and TypeScript files

---
+++ src/BestGarden.UI/src/app/components/support-form/support-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './support-form.component.html',
+  styleUrl: './support-form.component.scss'
+})
+export class SupportFormComponent {
+
+}

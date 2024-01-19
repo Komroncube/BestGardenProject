ded21855c158bbfae6d34eeaaae4e2def89c6878
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-19 21:49:32 +0500

    Add ImageUrlPipe to app.config.ts

--- src/BestGarden.UI/src/app/app.config.ts
+++ src/BestGarden.UI/src/app/app.config.ts
@@ -7,6 +7,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch } fro
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TokenInterceptor } from './services/interceptors/token.interceptor';
 import { ApiInterceptor } from './services/interceptors/api.interceptor';
+import { ImageUrlPipe } from './pipes/image-url.pipe';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,7 +18,9 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(BrowserAnimationsModule), 
     importProvidersFrom(RouterOutlet),
     { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true},
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true}
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
+    ImageUrlPipe
   ],
+  
   
 };

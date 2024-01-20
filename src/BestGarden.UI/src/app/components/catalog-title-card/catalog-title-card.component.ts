e6282a011db3ac013fce8d78f1c35b0a330a7ba0
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-20 21:49:12 +0500

    Update catalog title card component

--- src/BestGarden.UI/src/app/components/catalog-title-card/catalog-title-card.component.ts
+++ src/BestGarden.UI/src/app/components/catalog-title-card/catalog-title-card.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { ICatalogTitle } from '../../Interfaces/Catalogs/ICatalogTitle';
+import { RouterLink } from '@angular/router';
+import { ImageUrlPipe } from "../../pipes/image-url.pipe";
 
 
 @Component({
-  selector: 'app-catalog-title-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './catalog-title-card.component.html',
-  styleUrl: './catalog-title-card.component.scss'
+    selector: 'app-catalog-title-card',
+    standalone: true,
+    templateUrl: './catalog-title-card.component.html',
+    styleUrl: './catalog-title-card.component.scss',
+    imports: [RouterLink, ImageUrlPipe]
 })
 export class CatalogTitleComponent {
   @Input()

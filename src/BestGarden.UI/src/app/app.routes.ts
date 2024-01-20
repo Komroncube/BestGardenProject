134ffdbdf80057691f1a786a4bfcf57786e23904
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-20 21:50:17 +0500

    Update app.routes.ts to include new routes

--- src/BestGarden.UI/src/app/app.routes.ts
+++ src/BestGarden.UI/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PlaygroundComponent } from './pages/playground/playground.component';
+import { CatalogsComponent } from './pages/catalogs/catalogs.component';
+import { MainTemplateComponent } from './pages/main-template/main-template.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: '', component: PlaygroundComponent}
+    { 
+        path: 'home',
+        component: MainTemplateComponent,
+        loadChildren: () => import('./pages/main-template/main-template.routes')
+        .then(m => m.MAIN_TEMPLATE_ROUTES)
+    },
+    { path: '', component: PlaygroundComponent},
+    { path: '**', redirectTo: 'home' }
 ];

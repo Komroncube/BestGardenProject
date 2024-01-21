3dfb187804ced40a75b67fe6992aaf17ff3583e9
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-21 17:07:23 +0500

    Add product card component to playground page

--- src/BestGarden.UI/src/app/pages/playground/playground.component.ts
+++ src/BestGarden.UI/src/app/pages/playground/playground.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { FaqComponent } from '../../components/faq/faq.component';
 import { SupportFormComponent } from "../../components/support-form/support-form.component";
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
+import { IProduct } from '../../Interfaces/Products/IProduct';
 
 @Component({
     selector: 'app-playground',
     standalone: true,
     templateUrl: './playground.component.html',
     styleUrl: './playground.component.scss',
-    imports: [FaqComponent, SupportFormComponent]
+    imports: [FaqComponent, SupportFormComponent, ProductCardComponent]
 })
 export class PlaygroundComponent {
-
+    data : IProduct= {
+        id: 1,
+        name: 'Product 1',
+        description: 'Product 1 description',
+        price: 100,
+        imagePath: 'https://via.placeholder.com/150',
+    };
 }
+

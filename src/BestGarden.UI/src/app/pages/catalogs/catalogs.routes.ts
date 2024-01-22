bd7b5dd2d55ea98c1a1f8d2dab7e3a05b0490e3e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-22 19:21:15 +0500

    Remove catalogs.routes.ts

--- src/BestGarden.UI/src/app/pages/catalogs/catalogs.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from "@angular/router";
-import { CatalogsComponent } from "./catalogs.component";
-
-export const CATALOGS_ROUTES: Routes = [
-    { path: '', component: CatalogsComponent},
-    { path: ':id', component: CatalogsComponent}
-
-];

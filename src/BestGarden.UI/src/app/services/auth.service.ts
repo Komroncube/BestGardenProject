bf734ff6dd224f03567bf97f364d8bfc82b3b054
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-22 21:29:37 +0500

    Add AuthService and TokenService classes

---
+++ src/BestGarden.UI/src/app/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from "@angular/common/http";
+import { AuthRequest, IAuthResponse } from "../Interfaces/auth.interface";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
+@Injectable({
+    providedIn: 'root'
+  })
+export class AuthService {
+    constructor(private http: HttpClient) { }
+    authResponse? : IAuthResponse;
+    login(authReq: AuthRequest) : Observable<IAuthResponse|undefined> {
+        return this.http.post<IAuthResponse>('api/Users/authorize', authReq)
+    }
+}

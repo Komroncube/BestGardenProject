2649e0fd21ef7d27ec999848c998001a20e40890
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-22 19:20:24 +0500

    Add AuthenticateUserCommand and AuthenticateUserCommandHandler classes

---
+++ src/BestGarden.Application/UseCases/Users/Commands/AuthenticateUser/AuthenticateUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using BestGarden.Application.DTOs.Users;
+
+namespace BestGarden.Application.UseCases.Users.Commands.AuthenticateUser;
+public class AuthenticateUserCommand : ICommand<AuthenticationResponse>
+{
+    public AuthenticationRequest AuthRequest { get; set; }
+}
---
+++ src/BestGarden.Application/UseCases/Users/Commands/AuthenticateUser/AuthenticateUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using BestGarden.Application.DTOs.Users;
+
+namespace BestGarden.Application.UseCases.Users.Commands.AuthenticateUser;
+public class AuthenticateUserCommandHandler //: ICommandHandler<AuthenticateUserCommand, AuthenticationResponse>
+{
+    private readonly IUserRepository _userRepository;
+
+    public AuthenticateUserCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public Task<AuthenticationResponse> Handle(AuthenticateUserCommand request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new AuthenticationResponse());
+    }
+}

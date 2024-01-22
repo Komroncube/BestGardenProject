41e1634056c79b3a54db529dd7515fd44a6276e3
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-22 19:21:34 +0500

    Add quantity button and display total price

--- src/BestGarden.UI/src/app/pages/playground/playground.component.ts
+++ src/BestGarden.UI/src/app/pages/playground/playground.component.ts
@@ -3,13 +3,14 @@ import { FaqComponent } from '../../components/faq/faq.component';
 import { SupportFormComponent } from "../../components/support-form/support-form.component";
 import { ProductCardComponent } from "../../components/product-card/product-card.component";
 import { IProduct } from '../../Interfaces/Products/IProduct';
+import { QuantityButtonComponent } from "../../components/quantity-button/quantity-button.component";
 
 @Component({
     selector: 'app-playground',
     standalone: true,
     templateUrl: './playground.component.html',
     styleUrl: './playground.component.scss',
-    imports: [FaqComponent, SupportFormComponent, ProductCardComponent]
+    imports: [FaqComponent, SupportFormComponent, ProductCardComponent, QuantityButtonComponent]
 })
 export class PlaygroundComponent {
     data : IProduct= {
@@ -19,5 +20,6 @@ export class PlaygroundComponent {
         price: 100,
         imagePath: 'https://via.placeholder.com/150',
     };
+    num: number =1;
 }
 

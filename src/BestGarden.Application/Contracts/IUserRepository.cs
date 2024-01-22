02880f60b9efb07c644e2a65df09038fa91a23fa
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-22 19:19:25 +0500

    Add authentication method to IUserRepository and UserRepository

--- src/BestGarden.Application/Contracts/IUserRepository.cs
+++ src/BestGarden.Application/Contracts/IUserRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace BestGarden.Application.Contracts;
+﻿using BestGarden.Application.DTOs.Users;
+
+namespace BestGarden.Application.Contracts;
 public interface IUserRepository : IGenericRepository<User>
 {
     Task<User> GetUserByEmailAsync(string email, CancellationToken cancellationToken);
+    Task<User> AuthenticateAsync(AuthenticationRequest request, CancellationToken cancellationToken);
 }
--- src/BestGarden.Infrastructure/Repositories/UserRepository.cs
+++ src/BestGarden.Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BestGarden.Application.DTOs.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace BestGarden.Infrastructure.Repositories;
 
@@ -7,10 +8,16 @@ internal class UserRepository : GenericRepository<User>, IUserRepository
     private readonly ApplicationDbContext _dbContext;
     public UserRepository(ApplicationDbContext dbContext) : base(dbContext)
     {
+        _dbContext = dbContext;
     }
 
     public Task<User> GetUserByEmailAsync(string email, CancellationToken cancellationToken)
     {
         return _dbContext.Users.FirstOrDefaultAsync(u => u.Email == email, cancellationToken);
+    }
+
+    public Task<User> AuthenticateAsync(AuthenticationRequest request, CancellationToken cancellationToken)
+    {
+        return _dbContext.Users.FirstOrDefaultAsync(u => u.Email == request.Email && u.Password == request.Password, cancellationToken);
     }
 }

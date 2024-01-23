8e3caa17e70164567176348e4e45a1d96df3f0e3
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-23 21:51:44 +0500

    Add catalog service to footer component

--- src/BestGarden.UI/src/app/components/footer/footer.component.ts
+++ src/BestGarden.UI/src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ICatalogTitle } from '../../Interfaces/Catalogs/ICatalogTitle';
+import { CatalogsService } from '../../services/catalogs.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  catalogs: ICatalogTitle[] = [];
+  /**
+   *
+   */
+  constructor(private catalogService: CatalogsService) {
+    catalogService.getCatalogs().subscribe({
+      next: (catalogs) => {
+        this.catalogs = catalogs.slice(0, 5);
+        console.log(this.catalogs);
+      },
+      error: (error: any) => console.log(error)
+    });
+  }
 }

c6921d8ff5ae5f650ae871decef4d7bb2aaf4ffb
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-23 20:39:30 +0500

    Add NewsComponent to main-template.routes.ts

--- src/BestGarden.UI/src/app/pages/main-template/main-template.routes.ts
+++ src/BestGarden.UI/src/app/pages/main-template/main-template.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from "../home/home.component";
 import { MainTemplateComponent } from "./main-template.component";
 import { CatalogsComponent } from "../catalogs/catalogs.component";
 import { CatalogDetailsComponent } from "../catalogs/catalog-details/catalog-details.component";
+import { NewsComponent } from "../news/news.component";
 
 export const MAIN_TEMPLATE_ROUTES = [
     { 
@@ -18,6 +19,10 @@ export const MAIN_TEMPLATE_ROUTES = [
     {
         path: 'catalogs/:id',
         component: CatalogDetailsComponent
+    },
+    {
+        path: 'news',
+        component: NewsComponent,
     },
     {
         path: 'playground',

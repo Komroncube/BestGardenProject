72c950763d0248247f67cffb50453e0288278aed
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-26 18:49:50 +0500

    Add QuestionEntityConfiguration class for configuring the Question entity in the database.

---
+++ src/BestGarden.Infrastructure/Configurations/Entities/QuestionEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BestGarden.Infrastructure.Configurations.Entities;
+public class QuestionEntityConfiguration : IEntityTypeConfiguration<Question>
+{
+    public void Configure(EntityTypeBuilder<Question> builder)
+    {
+        builder.ToTable("Questions");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Email)
+            .HasMaxLength(100)
+            .IsRequired(true);
+        builder.Property(x=>x.Message)
+            .HasMaxLength(1000)
+            .IsRequired(true);
+        builder.Property(x=>x.FullName)
+            .HasMaxLength(50)
+            .IsRequired(true);
+        builder.Property(x => x.IsAnswered)
+            .HasDefaultValue(false)
+            .IsRequired();
+    }
+
+}

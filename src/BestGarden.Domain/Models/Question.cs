9ebc146f8b1429d32c623e61869006e49bdbd41e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-26 18:49:36 +0500

    Add Question model to BestGarden.Domain.Models

---
+++ src/BestGarden.Domain/Models/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace BestGarden.Domain.Models;
+public class Question : BaseDomainEntity
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+    public bool IsAnswered { get; set; }
+}

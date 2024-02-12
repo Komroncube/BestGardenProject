f75464c3164440869b267ffb20bd56dcce553b98
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-02-12 15:11:56 +0500

    Add BestGarden.SharedLibrary project and custom JWT layer

---
+++ src/BestGarden.SharedLibrary/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BestGarden.SharedLibrary;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCustomJwtLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(jwt =>
+        {
+            jwt.RequireHttpsMetadata = false;
+            jwt.SaveToken = true;
+            jwt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = configuration["JWT:ValidAudience"],
+                ValidIssuer = configuration["JWT:ValidIssuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["JWT:SecretKey"])),
+                ClockSkew = TimeSpan.Zero
+            };
+
+        });
+        return services;
+    }
+}
